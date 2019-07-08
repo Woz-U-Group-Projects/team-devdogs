3bf0a133d8c42a6c53dc5732d3a77fa179e63734
AgnorBook <mike.trula@gmail.com>
2019-07-07 21:28:03 -0400

    Movie item creation and front end prep for integration, index file prepped for movie list front page.

---
+++ src/app/models/movie.ts
@@ -0,0 +1,10 @@
+export class Movie {
+    id: number;
+    title: string;
+    director: string;
+    starring: string;
+    rating: number;
+    releaseDate: string;
+    genre: string;
+    
+}
---
+++ src/app/models/review.ts
@@ -0,0 +1,8 @@
+export class Review {
+    reviewID: number;
+    movieReview: string;
+    movieID: number;
+    userLoginID: number;
+    ratingID: number;
+    commentsID: number;
+}
