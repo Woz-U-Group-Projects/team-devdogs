c6d5cb5af64f6fd0c71716bec071069365ef0c16
Tracy Kidd <tracykidd@hotmail.co>
2019-07-30 00:03:03 -0700

    working nav

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
