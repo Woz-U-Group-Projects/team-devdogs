727f5557a418d06bd6b1b9f32a85151dd76d89c2
skanyavong <skanyavong@gmail.com>
2019-08-04 16:15:02 -0700

    movie page

--- client/src/app/movies/movies.component.ts
+++ client/src/app/movies/movies.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
-import { MovieService } from "../movie.service";
-import { Movie } from "../models/movie";
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+import { Movie } from '../models/movie';
 
 @Component({
   selector: 'app-movies',
