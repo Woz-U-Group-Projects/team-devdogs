20cedc4ea2b00e5f748a0c120aa7867c67716edf
skanyavong <skanyavong@gmail.com>
2019-08-04 18:10:35 -0700

    save app.movie

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,12 +6,6 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 
-
-// import { NgModule } from "@angular/core";
-// import { Routes, RouterModule } from "@angular/router";
-// import { HomeComponent } from "./home/home.component";
-
-// const routes: Routes = [{ path: "", component: HomeComponent }];
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'movies', component: MoviesComponent },
